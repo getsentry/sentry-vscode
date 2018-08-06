ec80b0866321b27d1c0c46f35d0166d2f72f5d77
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-08-06 20:43:29 +0200

    feat: re-add embedded debug server mode

--- src/debugger/index.ts
+++ src/debugger/index.ts
@@ -8,7 +8,9 @@ import {
   WorkspaceFolder,
 } from 'vscode';
 
+import * as Net from 'net';
 import { Event } from '../sentry';
+import { SentryDebugSession } from './server';
 
 export function startDebugging(event: Event): Thenable<boolean> {
   return debug.startDebugging(undefined, {
@@ -19,7 +21,15 @@ export function startDebugging(event: Event): Thenable<boolean> {
   });
 }
 
+/*
+ * Set the following compile time flag to true if the
+ * debug adapter should run inside the extension host.
+ * Please note: the test suite does no longer work in this mode.
+ */
+const EMBED_DEBUG_ADAPTER = true;
+
 export class SentryConfigurationProvider implements DebugConfigurationProvider {
+  private _server?: Net.Server;
   /**
    * Massage a debug configuration just before a debug session is being launched,
    * e.g. add all missing attributes to the debug configuration.
@@ -29,6 +39,20 @@ export class SentryConfigurationProvider implements DebugConfigurationProvider {
     config: DebugConfiguration,
     _token?: CancellationToken,
   ): ProviderResult<DebugConfiguration> {
+    if (EMBED_DEBUG_ADAPTER) {
+      // start port listener on launch of first debug session
+      if (!this._server) {
+        // start listening on a random port
+        this._server = Net.createServer(socket => {
+          const session = new SentryDebugSession();
+          session.setRunAsServer(true);
+          session.start(socket as NodeJS.ReadableStream, socket);
+        }).listen(0);
+      }
+
+      // make VS Code connect to debug server instead of launching debug adapter
+      config.debugServer = this._server.address().port;
+    }
     return config;
   }
 
