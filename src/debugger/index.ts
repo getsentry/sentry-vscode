9680f8915855bc59a12323bc640f559592e497aa
Jan Michael Auer <jan.auer@sentry.io>
2018-08-08 14:21:49 +0200

    fix: Properly dispose the SentryConfigurationProvider

--- src/debugger/index.ts
+++ src/debugger/index.ts
@@ -20,6 +20,50 @@ import { SentryDebugSession } from './server';
 const unlink = promisify(fs.unlink);
 const writeFile = promisify(fs.writeFile);
 
+/*
+ * Set the following compile time flag to true if the
+ * debug adapter should run inside the extension host.
+ * Please note: the test suite does no longer work in this mode.
+ */
+const EMBED_DEBUG_ADAPTER = true;
+
+class SentryConfigurationProvider implements DebugConfigurationProvider {
+  private _server?: Net.Server;
+
+  public resolveDebugConfiguration(
+    _folder: WorkspaceFolder | undefined,
+    config: DebugConfiguration,
+    _token?: CancellationToken,
+  ): ProviderResult<DebugConfiguration> {
+    if (!config.event) {
+      // abort launch
+      return window.showInformationMessage('Missing event payload').then(() => undefined);
+    }
+
+    if (EMBED_DEBUG_ADAPTER) {
+      // start port listener on launch of first debug session
+      if (!this._server) {
+        // start listening on a random port
+        this._server = Net.createServer(socket => {
+          const session = new SentryDebugSession();
+          session.setRunAsServer(true);
+          session.start(socket as NodeJS.ReadableStream, socket);
+        }).listen(0);
+      }
+
+      // make VS Code connect to debug server instead of launching debug adapter
+      config.debugServer = this._server.address().port;
+    }
+    return config;
+  }
+
+  public dispose(): void {
+    if (this._server) {
+      this._server.close();
+    }
+  }
+}
+
 async function createTempFile(contents: string): Promise<string> {
   const random = Math.random()
     .toString()
@@ -46,50 +90,6 @@ export async function startDebugging(event: Event): Promise<boolean> {
     });
   } finally {
     await unlink(tempFile);
-  }
-}
-
-/*
- * Set the following compile time flag to true if the
- * debug adapter should run inside the extension host.
- * Please note: the test suite does no longer work in this mode.
- */
-const EMBED_DEBUG_ADAPTER = true;
-
-export class SentryConfigurationProvider implements DebugConfigurationProvider {
-  private _server?: Net.Server;
-  /**
-   * Massage a debug configuration just before a debug session is being launched,
-   * e.g. add all missing attributes to the debug configuration.
-   */
-  public resolveDebugConfiguration(
-    _folder: WorkspaceFolder | undefined,
-    config: DebugConfiguration,
-    _token?: CancellationToken,
-  ): ProviderResult<DebugConfiguration> {
-    if (!config.event) {
-      return window.showInformationMessage('Cannot find a program to debug').then(_ => undefined); // abort launch
-    }
-
-    if (EMBED_DEBUG_ADAPTER) {
-      // start port listener on launch of first debug session
-      if (!this._server) {
-        // start listening on a random port
-        this._server = Net.createServer(socket => {
-          const session = new SentryDebugSession();
-          session.setRunAsServer(true);
-          session.start(socket as NodeJS.ReadableStream, socket);
-        }).listen(0);
-      }
-
-      // make VS Code connect to debug server instead of launching debug adapter
-      config.debugServer = this._server.address().port;
-    }
-    return config;
-  }
-
-  public dispose(): void {
-    return;
   }
 }
 
